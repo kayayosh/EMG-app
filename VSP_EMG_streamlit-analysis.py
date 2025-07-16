e8c0065670cf09b97fdbad27ce60317fb5ce788a
kayayosh <65183405+kayayosh@users.noreply.github.com>
2025-07-16 09:44:02 -0700

    Update VSP_EMG_streamlit-analysis.py

--- VSP_EMG_streamlit-analysis.py
+++ VSP_EMG_streamlit-analysis.py
@@ -3,20 +3,10 @@
 #create a streamlit app to show how EMG can be analysed
 
 
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import matplotlib.pyplot as plt
-import glob
-import plotly.io as pio
-from scipy.signal import butter, filtfilt
-import os
-from PyQt5 import QtWidgets
 import streamlit as st
-import neurokit2 as nk
-from datetime import datetime
+import pandas as pd
+import numpy as np
+import matplotlib 
 
 st.title('Analyze electromyography (EMG) data')
 
